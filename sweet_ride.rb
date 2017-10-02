1ad9a8b1bd74d25a1ba1a5ffb03f64eeb2acccad
andrel6392 <andrel6392@hstat.org>
2017-10-02 15:40:53 +0000

    sweet ride complete

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,20 @@
 #code your answers here
+def check_it_out 
+    return "Check out my sweet new ride, dude!"
+end
+
+def no_big_deal(car)
+    return "No big deal, it's a #{car}"
+end
+
+def stolen
+    return "dude, where's my car?"
+end
+
+def reporting_theft(stolen_car)
+    return "OFFICER, SOMEONE'S STOLEN MY #{stolen_car.upcase}"
+end
+
+def how_long_until_i_can_get_another_car(car_price, hourly_wage)
+    return car_price / hourly_wage
+end
